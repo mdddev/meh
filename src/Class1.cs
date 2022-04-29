7db2f53dd22401ee89b480767a7da8660b0c6b62
mdddev <33447232+mdddev@users.noreply.github.com>
2022-04-29 14:59:56 +0200

    feat: mewkmekewm

--- src/Class1.cs
+++ src/Class1.cs
@@ -5,4 +5,5 @@ public class Class1
     public int MyProperty { get; set; } 
     public int MyProperty2 { get; set; }
     public int MyProperty3 { get; set; }
+    public int MyProperty4 { get; set; }
 }
