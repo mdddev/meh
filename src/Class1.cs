7ac550a7814b628d7f857dfd77dbe9fd9856d350
mdddev <33447232+mdddev@users.noreply.github.com>
2022-04-29 12:24:43 +0200

    feat: something new

--- src/Class1.cs
+++ src/Class1.cs
@@ -1,11 +1,6 @@
-﻿namespace Mgiteh;
+﻿namespace Meh;
 
 public class Class1
 {
-    private static void _meh()
-    {
-        System.Console.WriteLine("me2s2222h");
-    }
-
-    public void Test1() => _meh();
+    public int MyProperty { get; set; }
 }
