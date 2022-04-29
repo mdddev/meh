ee4196765e6ad404b88b0dd69862cf30343d5465
mdddev <33447232+mdddev@users.noreply.github.com>
2022-04-29 12:23:07 +0200

    feat: initial commit

---
+++ src/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace Mgiteh;
+
+public class Class1
+{
+    private static void _meh()
+    {
+        System.Console.WriteLine("me2s2222h");
+    }
+
+    public void Test1() => _meh();
+}
---
+++ tests/UnitTest1.cs
@@ -0,0 +1,8 @@
+using Xunit;
+namespace Meh.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1() => Assert.True(true);
+}
