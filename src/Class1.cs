3e58d68acc7e5a9b37125ebd00f856cf0f959ac9
mdddev <33447232+mdddev@users.noreply.github.com>
2022-04-30 11:52:52 +0200

    fix: nkhiiuhe

--- src/Class1.cs
+++ src/Class1.cs
@@ -5,7 +5,7 @@ public class Class1
     public int MyProperty { get; set; } 
     public int MyProperty2 { get; set; }
     public int MyProperty3 { get; set; }
-    public int MyProperty4 { get; set; }
+    // public int MyProperty4 { get; set; }
     // public int MyProperty5 { get; set; }
     // public int MyProperty6 { get; set; }
     // public int MyProperty7 { get; set; }
