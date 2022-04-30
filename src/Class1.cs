b2031b5cb1330c57efd2def7baddc9e30432d7ad
mdddev <33447232+mdddev@users.noreply.github.com>
2022-04-30 16:12:13 +0200

    breaking: rfjjrfirjfj
    dhdehd
    dede
    BREAKING CHANGE

--- src/Class1.cs
+++ src/Class1.cs
@@ -2,14 +2,6 @@
 
 public class Class1
 {
-    public int MyProperty { get; set; } 
-    // public int MyProperty2 { get; set; }
-    // public int MyProperty3 { get; set; }
-    // public int MyProperty4 { get; set; }
-    // public int MyProperty5 { get; set; }
-    // public int MyProperty6 { get; set; }
-    // public int MyProperty7 { get; set; }
-    // public int MyProperty8 { get; set; }
-    // public int MyProperty9 { get; set; }
+    public int MyProperty { get; set; }
 
 }
