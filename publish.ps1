b67fc9a2be854f7ccb2f96d492d753a18a9a4647
mdddev <33447232+mdddev@users.noreply.github.com>
2022-05-01 14:27:38 +0200

    doc: added readme

--- publish.ps1
+++ publish.ps1
@@ -5,4 +5,4 @@ param(
 )
 
 dotnet nuget push ./src/bin/Release/"meh.$version.nupkg" --source https://nuget.pkg.github.com/mdddev/index.json --api-key $github
-dotnet nuget push ./src/bin/Release/"meh.$version.nupkg" --source https://api.nuget.org/v3/index.json --api-key $nuget
+# dotnet nuget push ./src/bin/Release/"meh.$version.nupkg" --source https://api.nuget.org/v3/index.json --api-key $nuget
